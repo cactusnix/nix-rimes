1d316958dfce14bfd0f2495c48eb2e0a65bc7166
cactusnix <cocalemontea@gmail.com>
2023-05-25 18:29:52 +0800

    feat: add lua scripts

---
+++ rime.lua
@@ -0,0 +1,8 @@
+-- 以词定字
+select_character = require("select_character")
+-- 辅助是否在用户词典里面
+is_in_user_dict = require("is_in_user_dict")
+-- 日期相关
+date_translator = require("date_translator")
+-- 数字 金额大写
+number_translator = require("number_translator")
